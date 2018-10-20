e26896576b22cb7d93e890b167b0e7efa32d5703
Juan Daniel Pichardo <jpichardopena@gmail.com>
2018-10-20 00:02:47 -0400

    Minor styling changes.

--- src/pages/about/about.js
+++ src/pages/about/about.js
@@ -1,24 +1,24 @@
 import React from 'react'
 import Layout from '../../components/layout'
+import { FaReact } from 'react-icons/fa'
 
 import './about.scss'
 
 const About = () => {
-  return (
-    <Layout>
+  return <Layout>
       <div className="about">
         <h1 className="about-heading">
           <span className="tags">{'<'}</span>
           About <span className="tags">{'/>'}</span>
         </h1>
         <p className="about-text">
-          My skills primarily lie in the front end, but I'm leveling my skillset
-          everyday.
+          My skills primarily lie in the front end, but I'm leveling my
+          skillset everyday and branching to other areas.
         </p>
-        <h3 className="about-subheading">Skills</h3>
+        <h3 className="about-subheading">Skills </h3>
+        <FaReact style={{color: 'orangered'}} />
       </div>
     </Layout>
-  )
 }
 
 export default About

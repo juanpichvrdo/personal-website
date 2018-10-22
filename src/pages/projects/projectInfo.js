d8e0fd906edd8110c2b1ee24c67dd96859bf1786
Juan Daniel Pichardo <jpichardopena@gmail.com>
2018-10-22 00:52:27 -0400

    Added projects component to project page.

---
+++ src/pages/projects/projectInfo.js
@@ -0,0 +1,32 @@
+import tasteDiscoveryImg from '../../images/tasteDiscovery.png'
+import LyricScrapper from '../../images/LyricScrapper.png'
+
+const projectInfo = [
+  {
+    imgPath: tasteDiscoveryImg,
+    projectName: 'Taste Discovery',
+    projectInfoText:
+      'Taste Discovery is Web app that allows users to find recommendations and helps them discover new music, movies, TV shows, books and games, based on what they like. Users can search all categories, or limit their search to one specific category. The app displays information about the recommendations and provides youtube videos relating to the recommendations when available. Information is fetch from TasteDive API.',
+    languajes: [
+      { className: 'icon-css3' },
+      { className: 'icon-react' },
+      { className: 'icon-netlify' },
+    ],
+    codeLink: 'https://github.com/juanpichvrdo/Taste-Discovery',
+    demoLink: 'https://tastediscovery.netlify.com/',
+  },
+  {
+    imgPath: LyricScrapper,
+    projectName: 'LyricScrapper',
+    projectInfoText:
+      'LyricSrapper is Web app that finds the lyrics of a song providing the name of the artist and .',
+    languajes: [
+      { className: 'icon-css3' },
+      { className: 'icon-react' },
+      { className: 'icon-netlify' },
+    ],
+    demoLink: 'https://tastediscovery.netlify.com/',
+  },
+]
+
+export default projectInfo
--- src/pages/projects/projects.js
+++ src/pages/projects/projects.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import Layout from '../../components/layout'
 
+import projectInfo from './projectInfo'
+
 import './projects.scss'
 
 const Projects = () => {
@@ -11,7 +13,67 @@ const Projects = () => {
           <span className="tags">{'<'}</span>
           Projects <span className="tags">{'/>'}</span>
         </h1>
-        <h3 className="projects-subheading">Thing's I've build: </h3>
+        <p className="projects-text" style={{ fontSize: '2rem' }}>
+          Here are some of the things I've build, all of these projects are
+          completly responsive:
+        </p>
+
+        {projectInfo.map(project => {
+          return (
+            <div className="projects-container">
+              <h3 className="projects-subheading">{project.projectName} </h3>
+              <img
+                src={project.imgPath}
+                alt={project.projectName}
+                style={{ width: '100%' }}
+              />
+              <p className="projects-text">{project.projectInfoText}</p>
+              <p className="projects-text" style={{ marginTop: '2rem' }}>
+                Made with:{' '}
+                {project.languajes.map(languaje => {
+                  return (
+                    <span
+                      className={languaje.className}
+                      style={{ fontSize: '3.5rem', marginLeft: '1rem' }}
+                    />
+                  )
+                })}
+              </p>
+              <div
+                style={{
+                  display: 'flex',
+                  flexDirection: 'column',
+                  justifyContent: 'center',
+                  alignItems: 'center',
+                }}
+              >
+                <a
+                  className="projects-link"
+                  href={project.demoLink}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  Code{' '}
+                  <span
+                    className="icon-github"
+                    style={{
+                      fontSize: '3rem',
+                      marginLeft: '.5rem',
+                    }}
+                  />
+                </a>
+                <a
+                  className="projects-link"
+                  href={project.demoLink}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  Demo
+                </a>
+              </div>
+            </div>
+          )
+        })}
       </div>
     </Layout>
   )

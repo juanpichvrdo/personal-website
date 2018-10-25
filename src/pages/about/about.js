b792a4a620a5c6fdee8935b20b2c29d50e2dc717
Juan Daniel Pichardo <jpichardopena@gmail.com>
2018-10-25 01:59:36 -0400

    Changed files location.

--- src/pages/about/about.js
+++ src/pages/about/about.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import Layout from '../../components/layout'
-import { skills, familiar, tools } from './skills'
+
+import { skills, familiar, tools } from '../../assets/information/skills'
 
 import './about.scss'
 
--- src/pages/projects/projects.js
+++ src/pages/projects/projects.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import Layout from '../../components/layout'
 
-import projectInfo from './projectInfo'
+import projectInfo from '../../assets/information/projectInfo' 
 
 import './projects.scss'
 
